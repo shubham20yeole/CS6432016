daf00cb84c872db0f0367eb8a4a81667f8e20f6c
Shubham Yeole <shubham20.yeole@gmail.com>
2016-10-01 14:58:42 -0400

    modified project

--- shubhamwebapp/src/main/webapp/WEB-INF/jsp/themes/js/homepage.js
+++ shubhamwebapp/src/main/webapp/WEB-INF/jsp/themes/js/homepage.js
@@ -22,13 +22,11 @@ $('#testimonial').carousel({
   $(function(){
 
         $("#typed").typed({
-            // strings: ["Typed.js is a <strong>jQuery</strong> plugin.", "It <em>types</em> out sentences.", "And then deletes them.", "Try it out!"],
             stringsElement: $('#typed-strings'),
             typeSpeed: 30,
             backDelay: 500,
             loop: false,
-            contentType: 'html', // or text
-            // defaults to false for infinite loop
+            contentType: 'html', 
             loopCount: false,
             callback: function(){ foo(); },
             resetCallback: function() { newTyped(); }
